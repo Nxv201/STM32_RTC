57b6413a2032142fd38e96a7099f0889b7339ad9
V4z <poi102qwe@gmail.com>
2023-09-24 15:33:38 +0700

    initial project

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32_RTC' 
+ * Target:  'STM32_RTC' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
